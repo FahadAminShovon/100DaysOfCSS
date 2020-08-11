431e50f525d235218d1ac385cc6324f2f5fae986
Fahad Shovon <bm.amin.fahad@gmail.com>
2020-08-11 18:56:30 +0600

    :bug:Add day 34

--- Day 34/app.js
+++ Day 34/app.js
@@ -1,10 +1,27 @@
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
+    const burger = document.querySelector(".burger");
     const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll(".nav-links li");
+    // Toggle nav
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
+        burger.classList.toggle('toggle')
+        console.log(burger)
+            navLinks.forEach((link, index) => {
+              if (link.style.animation) {
+                link.style.animation = ``;
+              } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${
+                  index / 7 + 0.5
+                }s`;
+              }
+            });
+            // Burger Animation
     })
+
+    // Amimate Links
+
 }
 
 navSlide();
