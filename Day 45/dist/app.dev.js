9a89a5c55984354aa3aa56256daf5b01e7d8379a
Fahad Shovon <bm.amin.fahad@gmail.com>
2020-08-23 02:25:52 +0600

    :heavy_plus_sign: Add day 45

---
+++ Day 45/dist/app.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var signUpButton = document.getElementById('signUp');
+var signInButton = document.getElementById('signIn');
+var container = document.getElementById('container');
+signUpButton.addEventListener('click', function () {
+  container.classList.add('right-panel-active');
+});
+signInButton.addEventListener("click", function () {
+  container.classList.remove("right-panel-active");
+});
